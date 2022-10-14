8ff456133531db267b350eb940e1f0c8d2e73e08
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-10-15 06:42:07 +0900

    add css and validation

--- react-login/src/App.js
+++ react-login/src/App.js
@@ -1,23 +1,57 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
 
 function App() {
+  const initialValues = { username: "", mailAddress: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value })
+    //console.log(formValues);
+    setFormErrors(validate(formValues))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //ロウイン情報を送信する。
+    //バリデーションチェックする。
+    setFormErrors(validate(formValues))
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "ユーザー名を入力してください。";
+    }
+    if (!values.mailAddress) {
+      errors.mailAddress = "メールアドレスを入力してください。";
+    }
+    if (!values.password) {
+      errors.password = "パスワードを入力してください。";
+    }
+  }
+
   return (
     <div className="formContainer">
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <h1>ログインフォーム</h1>
         <hr />
         <div className="uiform">
           <div className="formField">
             <label>ユーザー名</label>
-            <input type="text" placeholder="ユーザー名" name="username" />
+            <input type="text" placeholder="ユーザー名" name="username" onChange={(e) => handleChange(e)} />
+
           </div>
           <div className="formField">
             <label>メールアドレス</label>
-            <input type="text" placeholder="メールアドレス" name="mailAddress" />
+            <input type="text" placeholder="メールアドレス" name="mailAddress" onChange={(e) => handleChange(e)} />
           </div>
           <div className="formField">
             <label>パスワード</label>
-            <input type="text" placeholder="パスワード" name="password" />
+            <input type="text" placeholder="パスワード" name="password" onChange={(e) => handleChange(e)} />
           </div>
           <button className="submitButton">ログイン</button>
         </div>
